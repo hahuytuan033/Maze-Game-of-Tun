c279bc309626e6e70f1d34aafa63f410ece77c58
huytuan033 <hahuytuan033@gmail.com>
2024-03-30 04:38:43 +0700

    add key and enemies

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -34,6 +35,15 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D (Collision2D collision)
     {
+        if(collision.gameObject.tag== "Keys")
+        {
+            Destroy(collision.gameObject);
+        }
+        if(collision.gameObject.tag== "Enemies")
+        {
+            Debug.Log("Enemy Hit");
+        }
+        //nhảy ra khỏi tường
         if(collision.gameObject.tag== "Wall")
         {
             if (Input.GetKey(KeyCode.LeftArrow))
